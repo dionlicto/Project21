d33f2456eed05daa0f9511521297f4d44734d434
dionlicto <74904920+dionlicto@users.noreply.github.com>
2020-12-06 21:01:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var thickness,wall;
+var bullet,speed,weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+
+speed=random(1,90)
+weight=random(400,1500)
+thickness=random(22,50)
+
+bullet=createSprite(50,200,50,5)
+bullet.velocityX=speed;
+bullet.shapeColor=color(255)
+
+wall=createSprite(1200,200,thickness,height/2)
+wall.shapeColor=color(80,80,80)
+
+}
+
+function draw() {
+  background(0);
+  
+  
+bullet.collide(wall);
+ 
+  
+ 
+if(hasCollided(bullet,wall))
+{
+  var damage=0.5 * weight * speed * speed/(thickness*thickness*thickness);
+  bullet.velocityX=0;
+
+if(damage>10)
+{
+  wall.shapeColor=color(255,0,0);
+}
+
+if(damage<10)
+{
+
+  wall.shapeColor=color(0,255,0);
+}
+
+
+}
+  
+
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall)
+{
+
+  bulletRightEdge=lbullet.x+bullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+
+  return false;
+
+}
